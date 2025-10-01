244da6df1a79524af42590c4c60a83ac79afd8eb
Benedict Rosas <benedictrosas2@gmail.com>
2025-10-01 17:35:50 +0800

    Template

---
+++ src/main/java/com/profilewebsite/finalproject/QuizController.java
@@ -0,0 +1,21 @@
+package com.profilewebsite.finalproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class QuizController {
+
+        @GetMapping("/index")
+        public String index() {
+            return "index";
+        }
+        @GetMapping("/login")
+        public String login() {
+            return "login.html";
+        }
+        @GetMapping("/signup")
+        public String signpage() {
+            return "signup.html";
+        }
+    }
