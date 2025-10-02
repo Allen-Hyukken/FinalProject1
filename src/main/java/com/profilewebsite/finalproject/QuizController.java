39234bff6e5ed86e4592e38e7fba4fa117f3334f
Allen Carl Odang <arenacedecain@gmail.com>
2025-10-02 19:13:03 +0800

    template added

--- src/main/java/com/profilewebsite/finalproject/QuizController.java
+++ src/main/java/com/profilewebsite/finalproject/QuizController.java
@@ -10,12 +10,9 @@ public class QuizController {
         public String index() {
             return "index";
         }
+
         @GetMapping("/login")
         public String login() {
             return "login";
-        }
-        @GetMapping("/signup")
-        public String signin() {
-            return "signup";
         }
     }
